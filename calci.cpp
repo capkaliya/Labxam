f94506baf7e4c77d54c1bb4b9e97dd389d6551e2
basantsingh <basumystyle.com@gmail.com>
2019-12-06 12:37:34 +0000

    CALCI

---
+++ calci.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+    char op;
+    float num1,num2;
+    cout<< "Enter operator either + ot - or * or /:";
+    cin>>op;
+    cout<<"enter two operands:";
+    cin>>num1>>num2;
+    switch(op) {
+        case '+': cout<< num1+num2;
+        break;
+        case '-': cout<< num1-num2;
+        break;
+        case '*': cout<<num1*num2;
+        break;
+        case '/': cout<< num1/num2;
+        break;
+        default:cout<< "Error! operator is not correct";
+        break;
+    }
+return 0;
+
+
+}
